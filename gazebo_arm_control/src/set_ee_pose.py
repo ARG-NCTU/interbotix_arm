945b28a8c255931db3641b6078440296e495dc61
wellyowo <welly.ee11@nycu.edu.tw>
2023-12-18 11:15:46 +0800

    Fix: add keyboard control and rosservice for testing

--- gazebo_arm_control/src/set_ee_pose.py
+++ gazebo_arm_control/src/set_ee_pose.py
@@ -13,7 +13,7 @@ from sensor_msgs.msg import JointState
 
 
 class gazebo_interbotix_sdk_bridge:
-    def __init__(self, robot_name='wx200'):
+    def __init__(self, robot_name='wx200', use_sim=True):
         #pose to joint_state using interbotix_sdk
         self.sub_end_effector_pose = rospy.Subscriber("/end_effector_pose", Pose, self.pose_callback, queue_size=1)
         self.pub_joint_state_from_catersian  = rospy.Publisher("/joint_states_from_api", JointState, queue_size=1)
@@ -21,19 +21,19 @@ class gazebo_interbotix_sdk_bridge:
         if (self.bot.arm.group_info.num_joints < 5):
             print('This demo requires the robot to have at least 5 joints!')
             sys.exit()
+        self.use_sim = use_sim
+
+        #self.sub_jointstate = rospy.Subscriber("/joint_states_from_api", JointState, self.jointstate_callback, queue_size=1)
+        if self.use_sim:
+            #publish message to gazebo_robot (for position_control)
+            self.pub_right_finger_gripper = rospy.Publisher("/" + robot_name + "/right_finger_controller/command",Float64,queue_size=1)
+            self.pub_left_finger_gripper = rospy.Publisher("/" + robot_name + "/left_finger_controller/command",Float64,queue_size=1)
+            self.pub_waist = rospy.Publisher("/" + robot_name + "/waist_controller/command",Float64,queue_size=1)
+            self.pub_elbow = rospy.Publisher("/" + robot_name + "/elbow_controller/command",Float64,queue_size=1)
+            self.pub_shoulder = rospy.Publisher("/" + robot_name + "/shoulder_controller/command",Float64,queue_size=1)
+            self.pub_wrist_angle = rospy.Publisher("/" + robot_name + "/wrist_angle_controller/command",Float64,queue_size=1)
+            self.pub_wrist_rotate = rospy.Publisher("/" + robot_name + "/wrist_rotate_controller/command",Float64,queue_size=1)
         
-        #joint_state to gazebo arm
-        self.sub_jointstate = rospy.Subscriber("/joint_states_from_api", JointState, self.jointstate_callback, queue_size=1)
-        #publish message to gazebo_robot (for position_control)
-        self.pub_right_finger_gripper = rospy.Publisher("/" + robot_name + "/right_finger_controller/command",Float64,queue_size=1)
-        self.pub_left_finger_gripper = rospy.Publisher("/" + robot_name + "/left_finger_controller/command",Float64,queue_size=1)
-        self.pub_waist = rospy.Publisher("/" + robot_name + "/waist_controller/command",Float64,queue_size=1)
-        self.pub_elbow = rospy.Publisher("/" + robot_name + "/elbow_controller/command",Float64,queue_size=1)
-        self.pub_shoulder = rospy.Publisher("/" + robot_name + "/shoulder_controller/command",Float64,queue_size=1)
-        self.pub_wrist_angle = rospy.Publisher("/" + robot_name + "/wrist_angle_controller/command",Float64,queue_size=1)
-        self.pub_wrist_rotate = rospy.Publisher("/" + robot_name + "/wrist_rotate_controller/command",Float64,queue_size=1)
-        
-        start = rospy.Service("/initial", Trigger, self.initial)
 
     def pose_callback(self, pose_data):
         joint_state = JointState()
@@ -42,7 +42,9 @@ class gazebo_interbotix_sdk_bridge:
             print(joint_angle_list)
             joint_state.header.stamp = joint_state.header.stamp = rospy.Time.now()
             joint_state.position = joint_angle_list
-            self.pub_joint_state_from_catersian.publish(joint_state)
+            #self.pub_joint_state_from_catersian.publish(joint_state)
+            if self.use_sim:
+                self.pub_arm(joint_state.position)
         else:
             print("No valid solution")
         print("------------------------------------------------------------------")
@@ -54,8 +56,8 @@ class gazebo_interbotix_sdk_bridge:
         self.pub_wrist_angle.publish(joint_value[3])
         self.pub_wrist_rotate.publish(joint_value[4])
 
-    def jointstate_callback(self, joint_data):
-        self.pub_arm(joint_data.position)
+    # def jointstate_callback(self, joint_data):
+    #     self.pub_arm(joint_data.position)
     
 
     def control_end_effector(self, key):
@@ -78,26 +80,15 @@ class gazebo_interbotix_sdk_bridge:
 
         joint_angle_list, find_ans = self.bot.arm.set_relative_ee_position_wrt_to_base_frame(dx=dx, dy=dy, dz=dz, execute=True, moving_time=0.1, accel_time=0.1, blocking=True)
         if find_ans:
-                print(joint_angle_list)
-                joint_state.header.stamp = joint_state.header.stamp = rospy.Time.now()
-                joint_state.position = joint_angle_list
-                self.pub_joint_state_from_catersian.publish(joint_state)
+            print(joint_angle_list)
+            joint_state.header.stamp = joint_state.header.stamp = rospy.Time.now()
+            joint_state.position = joint_angle_list
+            #self.pub_joint_state_from_catersian.publish(joint_state)
+            if self.use_sim:
+                self.pub_arm(joint_state.position)
         else:
             print("No valid solution")
         print("------------------------------------------------------------------")
-
-    def initial(self, req):
-        res = TriggerResponse()
-        try:
-            joint_value =[0.0, 0.8, -1.0, 0.0, 0.0] #for upside down case
-            # joint_value =[0.0, -1.5, 2.0, 0.0, 0.0]
-            self.pub_arm(joint_value)
-            res.success = True
-        except (rospy.ServiceException, rospy.ROSException) as e:
-            res.success = False
-            print("Service call failed: %s"%e)
-        return res
-
 
 
    
@@ -106,13 +97,18 @@ if __name__  == "__main__":
     rospy.init_node("gazebo_sdk_bridge")
     #robot_name = "wx250"
     robot_name = rospy.get_param("~robot_name")
-    gazebo_control = gazebo_interbotix_sdk_bridge(robot_name)
+    use_sim = rospy.get_param("~use_sim")
+    gazebo_control = gazebo_interbotix_sdk_bridge(robot_name, use_sim)
+    key_board_control = rospy.get_param("~keyboard_control")
     while not rospy.is_shutdown():
-        key = raw_input("please input key: ").strip()
-        if key == 'q':
-            break
+        if key_board_control:
+            key = raw_input("please input key: ").strip()
+            if key == 'q':
+                break
+            else:
+                gazebo_control.control_end_effector(key)
         else:
-            gazebo_control.control_end_effector(key)
+            pass
         # pass
     #rospy.spin()
     
--- gazebo_arm_control/src/vrarm.py
+++ gazebo_arm_control/src/vrarm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 ################################
 # The interbotix_robot_arm in gazebo is different from the real one,this code is for unity to control gazebo wx250 robot arm.
 # joint_value from vr is ['waist (shoulder in vr)', 'shoulder (elbow in vr)', 'elbow (forearm in vr)', 'wrist_angle (wrist in vr)', 'wrist_rotate (gripper in vr)']
@@ -11,22 +11,24 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PoseStamped, Pose
 from std_srvs.srv import Trigger, TriggerResponse
 from sensor_msgs.msg import JointState
+from interbotix_xs_modules.arm import InterbotixManipulatorXS
 
 class gazebo_arm_control():
-    def __init__(self, robot_name="wx200"):
+    def __init__(self, robot_name="wx200", use_sim=True):
         #subscribe message from unity
         self.sub_primary = rospy.Subscriber("/vr/right/primarybutton", Bool, self.primarybutton_callback,queue_size=1)	#gripper open
         self.sub_second = rospy.Subscriber("/vr/right/secondarybutton", Bool, self.secondarybutton_callback,queue_size=1) #gripper close
         self.sub_jointstate = rospy.Subscriber("/joint_states_test", JointState, self.jointstate_callback, queue_size=1)
-        
-        #publish message to gazebo_robot (for position_control)
-        self.pub_right_finger_gripper = rospy.Publisher(f"/{robot_name}/right_finger_controller/command",Float64,queue_size=1)
-        self.pub_left_finger_gripper = rospy.Publisher(f"/{robot_name}/left_finger_controller/command",Float64,queue_size=1)
-        self.pub_waist = rospy.Publisher(f"/{robot_name}/waist_controller/command",Float64,queue_size=1)
-        self.pub_elbow = rospy.Publisher(f"/{robot_name}/elbow_controller/command",Float64,queue_size=1)
-        self.pub_shoulder = rospy.Publisher(f"/{robot_name}/shoulder_controller/command",Float64,queue_size=1)
-        self.pub_wrist_angle = rospy.Publisher(f"/{robot_name}/wrist_angle_controller/command",Float64,queue_size=1)
-        self.pub_wrist_rotate = rospy.Publisher(f"/{robot_name}/wrist_rotate_controller/command",Float64,queue_size=1)
+        self.use_sim = use_sim
+        if self.use_sim:  
+            #publish message to gazebo_robot (for position_control)
+            self.pub_right_finger_gripper = rospy.Publisher("/" + robot_name + "/right_finger_controller/command",Float64,queue_size=1)
+            self.pub_left_finger_gripper = rospy.Publisher("/" + robot_name + "/left_finger_controller/command",Float64,queue_size=1)
+            self.pub_waist = rospy.Publisher("/" + robot_name + "/waist_controller/command",Float64,queue_size=1)
+            self.pub_elbow = rospy.Publisher("/" + robot_name + "/elbow_controller/command",Float64,queue_size=1)
+            self.pub_shoulder = rospy.Publisher("/" + robot_name + "/shoulder_controller/command",Float64,queue_size=1)
+            self.pub_wrist_angle = rospy.Publisher("/" + robot_name + "/wrist_angle_controller/command",Float64,queue_size=1)
+            self.pub_wrist_rotate = rospy.Publisher("/" + robot_name + "/wrist_rotate_controller/command",Float64,queue_size=1)
 
         #rosservice for initial position and special pose
         start = rospy.Service("/initial", Trigger, self.initial)
@@ -34,12 +36,18 @@ class gazebo_arm_control():
         gripper_open = rospy.Service("/gripper_open", Trigger, self.gripper_open)
         gripper_close = rospy.Service("/gripper_close", Trigger, self.gripper_close)
         push = rospy.Service("/push", Trigger, self.push)
+
+        self.bot = InterbotixManipulatorXS(robot_model = robot_name, group_name = "arm", gripper_name = "gripper", init_node = False, moving_time = 0.5, accel_time=0.3)
+
     def initial(self, req):
         res = TriggerResponse()
         try:
             #joint_value =[0.0, 0.8, -1.0, 0.0, 0.0] #for upside down case
-            joint_value =[0.0, -1.5, 2.0, 0.0, 0.0]
-            self.pub_arm(joint_value)
+            joint_value = [0.0009009980741545576, -1.3184024200773958, 1.617598039350118, -0.30828725668373025, -0.00013571852838190068]
+            if self.use_sim:
+                self.pub_arm(joint_value)
+            else:
+                self.bot.arm.set_joint_positions(joint_value)
             res.success = True
         except (rospy.ServiceException, rospy.ROSException) as e:
             res.success = False
@@ -49,17 +57,25 @@ class gazebo_arm_control():
         res = TriggerResponse()
         try:
             joint_value = [0.0, 0.8, -1.0, 0.0, 0.0]
-            self.pub_arm(joint_value)
+            if self.use_sim:
+                self.pub_arm(joint_value)
+            else:
+                self.bot.arm.set_joint_positions(joint_value)
             res.success = True
         except (rospy.ServiceException, rospy.ROSException) as e:
             res.success = False
             print("Service call failed: %s"%e)
         return res
-    # testing service 
+
+        
     def gripper_open(self, req):
         res = TriggerResponse()
         try:
-            self.pub_finger("open")
+            if self.use_sim:
+                self.pub_finger("open")
+            else:
+                self.bot.gripper.open()
+            
             res.success = True
         except (rospy.ServiceException, rospy.ROSException) as e:
             res.success = False
@@ -69,7 +85,10 @@ class gazebo_arm_control():
     def gripper_close(self, req):
         res = TriggerResponse()
         try:
-            self.pub_finger("close")
+            if self.use_sim:
+                self.pub_finger("close")
+            else:
+                self.bot.gripper.close()
             res.success = True
         except (rospy.ServiceException, rospy.ROSException) as e:
             res.success = False
@@ -79,17 +98,13 @@ class gazebo_arm_control():
     def push(self, req):
         res = TriggerResponse()
         try:
-            joint_value = [0.0, -0.4, 0.25, 0.0, 0.0]
-            self.pub_arm(joint_value)
-            time.sleep(0.3)
-            joint_value = [0.0, -0.8, 0.5, 0.0, 0.0]
-            self.pub_arm(joint_value)
-            time.sleep(0.3)
-            joint_value = [0.0, -0.4, 0.25, 0.0, 0.0]
-            self.pub_arm(joint_value)
-            time.sleep(0.3)
-            joint_value = [0.0, 0.8, -1.0, 0.0, 0.0]
-            self.pub_arm(joint_value)
+            joint_value_list = [[0.0, -0.4, 0.25, 0.0, 0.0], [0.0, -0.8, 0.5, 0.0, 0.0], [0.0, -0.4, 0.25, 0.0, 0.0], [0.0, 0.8, -1.0, 0.0, 0.0]]
+            for joint_value in joint_value_list:
+                if self.use_sim:
+                    self.pub_arm(joint_value)
+                else:
+                    self.bot.arm.set_joint_positions(joint_value)
+                time.sleep(0.5)
             res.success = True
         except (rospy.ServiceException, rospy.ROSException) as e:
             res.success = False
@@ -116,21 +131,39 @@ class gazebo_arm_control():
 
     def primarybutton_callback(self, msg):
         if msg.data == True:
-            self.pub_finger("open")
+            if self.use_sim:
+                self.pub_finger("open")
+            else:
+                self.bot.gripper.open()
         else:
             pass
 
     def secondarybutton_callback(self, msg):
         if msg.data == True:
-            self.pub_finger("close")
+            if self.use_sim:
+                self.pub_finger("close")
+            else:
+                self.bot.gripper.close()
         else:
             pass
     def jointstate_callback(self, joint_data):
-        self.pub_arm(joint_data.position)
+        if self.use_sim:
+            self.pub_arm(joint_data.position)
+        else:
+            self.bot.arm.set_joint_positions(joint_data.position)
         #print(self.joint_value)
 
 if __name__ == "__main__":
     rospy.init_node("vr_gazebo_arm_control")
     robot_name = rospy.get_param("~robot_name")
-    gazebo_arm_control(robot_name)
+    sim = rospy.get_param("~use_sim")
+    initial = rospy.get_param("~initial")
+    gazebo_arm_control(robot_name, sim)
+    if initial:
+        rospy.wait_for_service("/initial")
+        try:
+            initial = rospy.ServiceProxy("/initial", Trigger)
+            initial()
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
     rospy.spin()
